34b08d35834aa10c811dae1c3098f9b5ba33d433
ousmanedok <83321069+ousmanedok@users.noreply.github.com>
2021-11-25 02:40:32 +0000

    In 55 service layer get all articles (#29)
    * Created getAllArticles method in service layer

--- capstone-server/src/main/kotlin/com/capstone/server/content/service/ArticlesService.kt
+++ capstone-server/src/main/kotlin/com/capstone/server/content/service/ArticlesService.kt
@@ -1,21 +1,30 @@
 package com.capstone.server.content.service
 
 import com.capstone.server.content.domain.AddArticleUseCase
+import com.capstone.server.content.domain.GetAllArticlesUseCase
 import com.capstone.server.content.service.dto.ArticleRequest
 import com.capstone.server.content.service.dto.ArticleResponse
 import org.springframework.stereotype.Service
 
 interface ArticlesService {
     fun addArticle(request: ArticleRequest.AddArticleRequest): ArticleResponse
+    fun getAllArticles(request: ArticleRequest.GetAllArticlesRequest): ArticleResponse
 }
 
 @Service("articlesService")
-internal class ArticlesServiceImpl(val addArticleUseCase: AddArticleUseCase): ArticlesService{
+internal class ArticlesServiceImpl(
+    val addArticleUseCase: AddArticleUseCase,
+    val getAllArticlesUseCase: GetAllArticlesUseCase
+    ): ArticlesService{
     override fun addArticle(request: ArticleRequest.AddArticleRequest): ArticleResponse {
         return when (val response = addArticleUseCase.invoke(request)){
             is ArticleResponse.InvalidResponse -> throw response.exception
             else -> response
         }
+    }
+
+    override fun getAllArticles(request: ArticleRequest.GetAllArticlesRequest): ArticleResponse {
+        return getAllArticlesUseCase(request)
     }
 
 }
