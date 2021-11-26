bb108848cf01a80515b0fd21c0a0371ad6adf1bf
ousmanedok <83321069+ousmanedok@users.noreply.github.com>
2021-11-26 06:19:43 +0000

    In 55 service layer update article (#30)
    * Created updateArticle method in service layer

--- capstone-server/src/main/kotlin/com/capstone/server/content/service/ArticlesService.kt
+++ capstone-server/src/main/kotlin/com/capstone/server/content/service/ArticlesService.kt
@@ -2,6 +2,7 @@ package com.capstone.server.content.service
 
 import com.capstone.server.content.domain.AddArticleUseCase
 import com.capstone.server.content.domain.GetAllArticlesUseCase
+import com.capstone.server.content.domain.UpdateArticleUseCase
 import com.capstone.server.content.service.dto.ArticleRequest
 import com.capstone.server.content.service.dto.ArticleResponse
 import org.springframework.stereotype.Service
@@ -9,12 +10,14 @@ import org.springframework.stereotype.Service
 interface ArticlesService {
     fun addArticle(request: ArticleRequest.AddArticleRequest): ArticleResponse
     fun getAllArticles(request: ArticleRequest.GetAllArticlesRequest): ArticleResponse
+    fun updateArticle(request: ArticleRequest.UpdateArticleRequest): ArticleResponse
 }
 
 @Service("articlesService")
 internal class ArticlesServiceImpl(
     val addArticleUseCase: AddArticleUseCase,
-    val getAllArticlesUseCase: GetAllArticlesUseCase
+    val getAllArticlesUseCase: GetAllArticlesUseCase,
+    val updateArticleUseCase: UpdateArticleUseCase
     ): ArticlesService{
     override fun addArticle(request: ArticleRequest.AddArticleRequest): ArticleResponse {
         return when (val response = addArticleUseCase.invoke(request)){
@@ -25,6 +28,13 @@ internal class ArticlesServiceImpl(
 
     override fun getAllArticles(request: ArticleRequest.GetAllArticlesRequest): ArticleResponse {
         return getAllArticlesUseCase(request)
+    }
+
+    override fun updateArticle(request: ArticleRequest.UpdateArticleRequest): ArticleResponse {
+        return when (val response = updateArticleUseCase(request)){
+            is ArticleResponse.InvalidResponse -> throw response.exception
+            else -> response
+        }
     }
 
 }
