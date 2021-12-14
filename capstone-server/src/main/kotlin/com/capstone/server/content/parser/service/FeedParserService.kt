27dfaaa7cb0d94ae7b1b4a25c246e641ff3e94a8
bahbahtech <bahbahtech@users.noreply.github.com>
2021-12-14 10:06:45 -0500

    sanitize feedparser (#39)

--- capstone-server/src/main/kotlin/com/capstone/server/content/parser/service/FeedParserService.kt
+++ capstone-server/src/main/kotlin/com/capstone/server/content/parser/service/FeedParserService.kt
@@ -5,13 +5,10 @@ import com.capstone.server.content.parser.rome.FeedParser
 import org.springframework.stereotype.Service
 
 interface FeedService {
-    fun getArticles(feedUrl: String): List<Article>
+ // TODO
 }
 
 @Service("feedService")
 internal class FeedServiceImpl(val feedParser: FeedParser) : FeedService {
-    override fun getArticles(feedUrl: String): List<Article> {
-       return feedParser(feedUrl)
-    }
-
+    //TODO
 }
